64985d4c39fa487939ab004aa945f7d82841757c
Chris Cannon <chris.c.canon@gmail.com>
2018-03-20 13:37:55 -0400

    Create LevelData class

---
+++ MeteorBlasterBackend/src/meteorblasterbackend/LevelData.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meteorblasterbackend;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author CCannon
+ */
+public class LevelData {
+    private ArrayList<Level> levels;
+    
+    public LevelData() {
+        levels = new ArrayList<>();
+    }
+}
