7337e0373862e6d3ccbddc49fc902ec21cb52992
Chris Cannon <chris.c.canon@gmail.com>
2018-03-20 14:20:49 -0400

    Create rankdata class

---
+++ MeteorBlasterBackend/src/meteorblasterbackend/RankData.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meteorblasterbackend;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author CCannon
+ */
+public class RankData {
+    private ArrayList<Rank> ranks;
+    
+    public int getNumRanks() {
+        return ranks.size();
+    }
+    
+    public void addRank(Rank rank) {
+        ranks.add(rank);
+    }
+    
+    public void setRank(int index, Rank rank) { 
+        ranks.set(index, rank);
+    }
+    
+    public Rank getRank(int index) {
+        return ranks.get(index);
+    }
+    
+    public Rank deleteRank(int index) {
+        return ranks.remove(index);
+    }
+}
